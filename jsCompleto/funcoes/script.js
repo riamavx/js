900ece7eff3b38cc03c4eca192fee4ee736a6233
Maria Souza <messenger.mariah2017@gmail.com>
2020-11-23 19:36:30 -0300

    funcoes: exercicios resolvidos

---
+++ jsCompleto/funcoes/script.js
@@ -0,0 +1,79 @@
+// Crie uma função para verificar se um valor é Truthy
+// let idade = Number(prompt("Digite sua idade"))
+// const verificador = ()=>{
+//     if(idade >= 60){
+//         return true
+//     }else{
+//         return false
+//     }
+
+// }
+
+// console.log(verificador());
+
+//OU
+
+// function verificandoIdade(idade){
+
+//     if( typeof idade != 'number'){
+//         return "Por favor digite sua idade com um número"
+//     }else if(idade >= 60){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// console.log(verificandoIdade(65))
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+// const calculoPerimetro = (lados)=>{
+//     return lados*lados
+// }
+
+// console.log(calculoPerimetro(4))
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+// const nomeCompleto = (nome, sobrenome)=>{
+//     return nome + sobrenome
+// }
+
+// console.log(nomeCompleto("Maria ","Souza"))
+
+
+// Crie uma função que verifica se um número é par
+// const verificadorNumeroPar = (numero)=>{
+//     if(numero%2 === 0){
+//         console.log("numero par")
+//     }else{
+//         console.log("numero impar")
+//     }
+// }
+
+//  console.log(verificadorNumeroPar(0))
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
+// function precisoVisitar(paisesVisitados) {
+//     let totalPaises = 193;
+//     return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+//   }
+
+//   function jaVisitei(paisesVisitados) {
+//     let totalPaises = 193;
+//     return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+//   }
+//   console.log(precisoVisitar(20));
+//   console.log(jaVisitei(20));
+  
